885d15504bef2cd04ed1ccf1a6c6becb2b418c64
Dad <Dad>
2016-07-20 18:42:02 -0400

    adding jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,7 +29,7 @@
 //global
 var player = -1;
 var opponent = -1;
-var round = 0;
+var round = 1;
 //populate players 
 $(document).ready(function() {
 	//functions
@@ -43,17 +43,36 @@ $(document).ready(function() {
 	function displayWizards(){
 		$("#charStart").empty();
 		var first=0;
+		var	testBool=false,testBool2=false;
 		for (var i = 0; i < wizards.length; i++) {
-			if(i!==player&&i!==opponent){
+			//console.log("player selected" +player +" opponent selected"+opponent)
+			//for testing purposes only
+			if (i!=player) {
+				testBool=true;
+				//console.log(testBool)
+			}
+			else{
+				testBool=false;
+			}
+			if (i!=opponent) {
+				testBool2=true;
+				//console.log(testBool)
+			}
+			else{
+				testBool2=false;
+			}
+			console.log(i+"!="+player+": "+ testBool+" "+ i+"!="+opponent+""+testBool2)
+			//end of tests
+			if(i!=player&&i!=opponent){
 				var newWiz = $("<div/>").addClass("wizard col-sm-2").attr('wizard-id', i).html('<span class="name">'+ wizards[i].name +'</span><img class="imgWiz" src="assets/images/'+ wizards[i].image +'"><span class="points">'+ wizards[i].health +'</span>');
-				console.log("displaying: " +wizards[i].name);
+				//console.log("displaying: " +wizards[i].name);
 				if(first===0){
-					console.log("displaying: " +wizards[i].name+" this is the first one");
+					//console.log("displaying: " +wizards[i].name+" this is the first one");
 					$("#charStart").append(newWiz.addClass("col-sm-offset-1"));
 					first++;
 				}
 				else{
-					console.log("displaying: " +wizards[i].name);
+					//console.log("displaying: " +wizards[i].name);
 					$("#charStart").append(newWiz);
 				}
 
@@ -65,31 +84,49 @@ $(document).ready(function() {
 		else if (opponent<0) {
 			$(".rounds").html("<h1>Select your opponent</h1>");
 		}
+		$(".wizard").on("click",function(){
+		//select a character with the id of the clicked character
+		selectWizard(this.getAttribute("wizard-id"))
+	})
 	}
+
 	function selectWizard(index){
 		//console.log(wizards[index].name+" was Selected");
 		//checks to see if a player has been selected
-		if(player<0){
+
+		if(player===-1&&opponent===-1){
+			console.log("ADDING PLAYER")
 			//if they have not put in the player's index 
 			player=index;
 			//place them on the dueling board
-			placePlayer();
+			placePlayer(index);
 		}
 		//checks to see if a opponent has been selected
-		else if(opponent<0){
+		else if(opponent===-1){
+			console.log("ADDING Opponent")
 			//if they have not put in the opponents's index 
 			opponent=index;
 			//place them on the dueling board
-			placePlayer();
+			placePlayer(index);
 		}
 	}
-	function placePlayer(){
-		if(opponent<0){
-
+	function placePlayer(index){
+		//if the player has been placed then this should be call for the opponent
+		displayWizards();
+		if(opponent!==-1){
+			var newWiz = $("<div/>").addClass("opponent col-sm-3").attr('wizard-id', index).html('<span class="name">'+ wizards[index].name +'</span><img class="imgWiz" src="assets/images/'+ wizards[index].image +'"><span class="points">'+ wizards[index].health +'</span>');
+				console.log("displaying: " +wizards[index].name);
+				$('#opponent').html(newWiz);
+				$(".rounds").html("<h1>Round: "+round+"</h1>");
 		}
+		//otherwise we are placing the player
 		else{
-			
+			var newWiz = $("<div/>").addClass("player col-sm-offset-2 col-sm-3").attr('wizard-id', index).html('<span class="name">'+ wizards[index].name +'</span><img class="imgWiz" src="assets/images/'+ wizards[index].image +'"><span class="points">'+ wizards[index].health +'</span>');
+				console.log("displaying: " +wizards[index].name);
+				$('#player').html(newWiz);
+				$('#versus').html('<h1>Attack</h1>');
 		}
+		//displayWizards();
 	}
 
 
@@ -105,8 +142,5 @@ $(document).ready(function() {
 	//console.log("displaying");
 	displayWizards();
 	//set on click for player and opponent
-	$(".wizard").on("click",function(){
-		//select a character with the id of the clicked character
-		selectWizard(this.getAttribute("wizard-id"))
-	})
+	
 });
